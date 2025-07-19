d7eae147dc207d72d0ba353c6812583d6933daae
MaherMahmoudSafwat <MaherMsSoftware2001@gmail.com>
2025-07-19 18:21:56 +0300

    Add a new project

---
+++ FullInventorySystemProject/src/test/java/com/NewFullInventorySystemProductsProject/FullInventorySystemProject/FullInventorySystemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NewFullInventorySystemProductsProject.FullInventorySystemProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullInventorySystemProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
